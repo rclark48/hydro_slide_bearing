25de7171a520288d2a99756fd51bae159f4512ee
reedclark <reedclark@192.168.0.121>
2018-12-29 23:05:06 -0500

    initial commit

---
+++ functionTemplate.m
@@ -0,0 +1,37 @@
+function [out] = functionTemplate(in)
+%% Tittle Block:
+
+% Written by: Reed Clark
+% Date Created: 12-29-2018
+% Revised By: Reed Clark
+% Revision Description: initial commit
+
+%% Description:
+% Template for uniform functions.
+
+%% Inputs:
+% in: can be a scalar, vector, or matrix
+
+%% Outputs: 
+% out: can be a scalar, vector, or matrix
+
+%% Constants:
+% Define various constants/"variable declaration" to
+% facilitate easy downstream changes.
+
+%% Equations:
+% Define equations used throughout code in LaTex for use in publishing.
+
+% Block equation:
+%% 
+% 
+% $$e^{\pi i} + 1 = 0$$
+% 
+
+% Inline equation:
+%%
+% $x^2+e^{\pi i}$
+
+%% Code:
+out = in;
+end
