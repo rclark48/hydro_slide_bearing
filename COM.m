583b52297ce209c45014706b8b4ccbf019373c8d
Reed Clark <reedclark@Reeds-MacBook-Pro.local>
2019-01-04 16:31:55 -0500

    changed output from scalars to structure for % easier passage into other functions.

--- COM.m
+++ COM.m
@@ -1,10 +1,11 @@
-function [x_cm,z_cm,m] = COM(alpha, beta, const)
+function [com] = COM(alpha, beta, const)
 %% Tittle Block:
 
 % Written by: Reed Clark
 % Date Created: 12-30-2018
 % Revised By: Reed Clark
-% Revision Description: added const input and redefined the constants
+% Revision Description: changed output from scalars to structure for
+% easier passage into other functions.
 
 %% Description:
 % Computes the bearing center of mass coordinates 
@@ -21,11 +22,11 @@ alpha = inch2meter(alpha); % [in --> m]
 beta = inch2meter(beta); % [in --> m]
 
 %% Outputs: 
-% x_cm: scalar, [m], x-coordinate of the bearing center of mass
+% com.x: scalar, [m], x-coordinate of the bearing center of mass
 
-% z_cm: scalar, [m], z-coordinate of the bearing center of mass
+% com.z: scalar, [m], z-coordinate of the bearing center of mass
 
-% mass: scalar, [kg], mass of bearing
+% com.mass: scalar, [kg], mass of bearing
 
 %% Constants:
 A = const.A;
@@ -50,13 +51,16 @@ density = const.density;
 num_x = A*B^2 - alpha*beta^2;
 den_x = 2*(A*B - alpha*beta);
 x_cm = num_x./den_x;
+com.x = x_cm;
 
 % z_cm
 num_z = A^2*B/2 - A*alpha*beta + alpha^2*beta/2;
 den_z = A*B - alpha*beta;
 z_cm = num_z./den_z;
+com.z = z_cm;
 
 % mass
 m = density*L*(A*B - alpha*beta);
+com.mass = m;
 
 end
