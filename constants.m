9c296bb47522d0469cbbfdc55f4723aef9190e1e
Reed Clark <reedclark@lawn-128-61-65-123.lawn.gatech.edu>
2019-01-05 01:04:52 -0500

    added residual tolerance and time step

--- constants.m
+++ constants.m
@@ -4,7 +4,7 @@ function [const] = constants()
 % Written by: Reed Clark
 % Date Created: 12-30-2018
 % Revised By: Reed Clark
-% Revision Description: corrected output description
+% Revision Description: added residual tolerance and time step
 
 %% Description:
 % This is a modularization function that initializes all constants
@@ -30,7 +30,19 @@ const.z0 = 2e-3; % [m]
 const.density = 7750; %[kg/m^3], mild steel
 const.viscosity = 54.3e-3; %[Pa-s], soybean oil ~75 F
 
-const.err.gauss = eps; % relative error tolerance for Gauss-Seidel pressure solver
+% Relative percent error tolerance for Gauss-Seidel pressure solver
+const.err.gauss = eps;
+% Residual error tolerance for force and moment balance equations. The
+% residuals are expressed as a normalized approximate percent error
+% percentage, so the tolerance corresponds to roughly 1/10% error.
+const.err.resid = 1e-3; 
+
+% For most human timed events (such as this trial) are probably only
+% accurate to a hundreth of a second, so running the simulation at one
+% higher degree of resolution in the time domain should ensure that a
+% estimated travel time truncated to the hundreth of a second will be
+% a reasonable prediction.
+const.dt = 1e-3; % [s]
 
 %% Equations:
 % There are no formal equations to define in this function.
